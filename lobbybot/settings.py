1d36275b3c41531dd5c980714208dd93bb90b141
choicallum <choicallum@gmail.com>
2025-07-16 00:21:46 -0400

    update version and gitignore

--- lobbybot/settings.py
+++ lobbybot/settings.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 logger = logging.getLogger()
+VERSION = "2025/07/15 v2"
 
 load_dotenv()
 DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
@@ -13,7 +14,7 @@ USERS_PATH = BASE_DIR / os.getenv("USERS_PATH")
 LOG_PATH = BASE_DIR / os.getenv("LOG_PATH")
 RESOURCES_PATH = BASE_DIR / os.getenv("RESOURCES_PATH")
 BUMP_LOBBY_CHANNEL_ID = int(os.getenv("BUMP_LOBBY_CHANNEL_ID"))
-VERSION = "2025/07/15 v1"
+
 
 logger.setLevel(logging.INFO)
 # logger.setLevel(logging.DEBUG)
