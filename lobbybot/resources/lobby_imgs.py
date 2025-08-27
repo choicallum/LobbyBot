f4baa81b10938679179a2782e047a78d602e474a
choicallum <choicallum@gmail.com>
2025-08-27 02:32:17 -0400

    delete redundant file

--- lobbybot/resources/lobby_imgs.py
+++
@@ -1,7 +0,0 @@
-LOBBY_GIFS = [
-    "this is not a valid gif"
-    "https://media.tenor.com/kw-zE0H3lSgAAAAM/wooper-roller-skates.gif",
-    "https://images.steamusercontent.com/ugc/850469405431254094/234037665EB621D5EFAE7D0EA438565A2D18283B/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-    "https://i.pinimg.com/originals/a6/75/b0/a675b01bc3b2abfc55bdb623a2bfb9be.gif",
-    "https://media.tenor.com/9R-jFcujk3MAAAAM/wooper-pokemon.gif"
-]
